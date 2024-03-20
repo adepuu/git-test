9967e1b6bceeeff017b3df5f583090e7e142d360
adepuu <ade.santito@gmail.com>
2024-03-20 10:05:08 +0700

    fix: add new variable

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,6 +8,10 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
 
+        int a;
+        int b;
+        int c;
+        int d;
 //        for (int i = 1; i <= 5; i++) {
 //            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
 //            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
